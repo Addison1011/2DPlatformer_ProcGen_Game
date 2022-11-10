ff51507eb8860fc56509deed5c4a8cb820cc1822
Addison1011 <an10-1999@hotmail.com>
2022-11-09 23:36:41 -0600

    Refined Character Controller
    - Added clamped fall velocity
    - Removed unused variables
    - Added comments
    - Refined player controller variable values for a smoother feeling when using playing

--- ProceduralGen_2D_Platformer/Assets/Scripts/PlayerController.cs
+++ ProceduralGen_2D_Platformer/Assets/Scripts/PlayerController.cs
@@ -6,18 +6,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D playerRigidbody;
+    private Collider2D playerCollider;
 
     [SerializeField] private LayerMask platformLayerMask;
 
-    private bool isGrounded;
-    private bool pressingSpace;
-    private bool isRunning;
+    private bool isGrounded {get; set;}
+    private bool pressingSpace {get; set;}
+    private float movementDirection { get; set; }
+
+    public float jumpForce;
+    public float fallForce;
+    public float maxFallSpeed;
+    public float frictionAmount;
 
 
-    private float movementDirection;
-    public float runSpeed;
-    public float walkSpeed;
+    public float movementSpeed;
     public float accelPower;
+    public float acceleration;
 
 
     private float jumpCoyoteTimeCounter;
@@ -25,24 +30,14 @@ public class PlayerController : MonoBehaviour
 
     public float jumpCoyoteTime;
     public float jumpBuffer;
-    public float jumpForce;
+    
 
 
-    public float acceleration;
-    public float fallForce;
-    public float frictionAmount;
-
-    private Vector3 colliderSize;
-    private Vector3 colliderCenter;
     // Start is called before the first frame update
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
-
-        colliderSize = GetComponent<Collider2D>().bounds.size;
-        colliderCenter = GetComponent<Collider2D>().bounds.center;
-
-
+        playerCollider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -54,13 +49,8 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         ApplyMovementInputs();
+        ClampFallSpeed();
     }
-
-
-
-
-
-
 
 
     public void ApplyMovementInputs()
@@ -73,31 +63,18 @@ public class PlayerController : MonoBehaviour
 
     public void Move()
     {
-        // set the maximum speed based on if the player is running or walking
-        float maxSpeed;
-        if (isRunning)
-        {
-            maxSpeed = runSpeed;
-
-        }
-        else
-        {
-            maxSpeed = walkSpeed;
-        }
-
-        float targetSpeed = maxSpeed * movementDirection; // get the target speed with direction
+        float targetSpeed = movementSpeed * movementDirection; // get the target speed with direction
         float speedDif = targetSpeed - playerRigidbody.velocity.x;
 
         // as the players velocity gets closer to the target speed(or speedDif -> 0), the movement speed gets lessened exponentially.
         // acceleration value hastens the time in which movement speed approaches the target speed linearly
         // accelPower value hastens exponentially
 
-
-        float movementSpeed = Mathf.Pow(Mathf.Abs(speedDif) * acceleration, accelPower) * Mathf.Sign(speedDif);
+        float calculatedForce = Mathf.Pow(Mathf.Abs(speedDif) * acceleration, accelPower) * Mathf.Sign(speedDif);
 
         // apply calculated force to the player
         playerRigidbody.AddForce(
-                new Vector2((movementSpeed * Time.deltaTime), 0));
+                new Vector2((calculatedForce * Time.deltaTime), 0));
 
         ApplyFriction();
 
@@ -106,13 +83,15 @@ public class PlayerController : MonoBehaviour
 
     private void ApplyFriction()
     {
-        //Check if grounded and not using lef/right inputs
+        //Check if grounded and not using left/right inputs
         if (movementDirection == 0 && IsGrounded())
         {
             // use friction ammount or velocity depending on which one is bigger, also creates a slow stop
             float negativeForceAmount = Mathf.Min(Mathf.Abs(playerRigidbody.velocity.x), Mathf.Abs(frictionAmount));
+
             //apply the correct direction
             negativeForceAmount *= Mathf.Sign(playerRigidbody.velocity.x);
+
             // applys force aginst movement direction
             playerRigidbody.AddForce(Vector2.right * -negativeForceAmount, ForceMode2D.Impulse);
         }
@@ -121,12 +100,20 @@ public class PlayerController : MonoBehaviour
 
     private void ApplyJumpVelocity()
     {
+        // zero out vertical velocity before adding jump velocity
+        playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, 0);
+        playerRigidbody.AddForce(new Vector2(0, jumpForce));
 
-        /*playerRigidbody.AddForce(
-                new Vector2(0, (4 * jumpForce * Time.deltaTime)));*/
-        // playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, 0);
-        playerRigidbody.velocity += (jumpForce * Time.deltaTime) * Vector2.up;
+        //playerRigidbody.velocity += jumpForce * Vector2.up;
 
+    }
+
+    private void ClampFallSpeed()
+    {
+        if (playerRigidbody.velocity.y <= -maxFallSpeed)
+        {
+            playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, -maxFallSpeed);
+        }
     }
 
 
@@ -144,11 +131,21 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    private void ApplyIncreasedFallSpeed()
+    private void ApplyFallSpeed()
     {
         if (!isGrounded)
         {
-            playerRigidbody.velocity += (fallForce * Time.deltaTime) * Vector2.down;
+            // If player is falling and the downward vertical velocity is greater than maxFallSpeed then clamp the fall speed
+            // Otherwise apply increased fall speed
+            if (playerRigidbody.velocity.y <= -maxFallSpeed)
+            {
+                playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, -maxFallSpeed);
+            }
+            else
+            {
+                playerRigidbody.velocity += (fallForce * Time.deltaTime) * Vector2.down;
+            }
+           
         }
     }
 
@@ -171,7 +168,7 @@ public class PlayerController : MonoBehaviour
 
         ApplyJumpCoyoteTime();
         ApplyJumpBuffer();
-        ApplyIncreasedFallSpeed();
+        ApplyFallSpeed();
 
 
 
@@ -200,15 +197,6 @@ public class PlayerController : MonoBehaviour
             pressingSpace = false;
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            isRunning = true;
-        }
-        else if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            isRunning = false;
-        }
-
         // - or + Value of the horizontal axis
         movementDirection = Input.GetAxisRaw("Horizontal");
 
@@ -216,18 +204,21 @@ public class PlayerController : MonoBehaviour
 
     public bool IsGrounded()
     {
-        // Create a ray box near bottom of player, return true if the ray is colliding with "Ground" LayerMask
+        
         float extraHeightTest = 0.1f;
-        Vector3 colliderSize = GetComponent<Collider2D>().bounds.size;
-        Vector3 colliderCenter = GetComponent<Collider2D>().bounds.center;
+        Vector3 colliderSize = playerCollider.bounds.size;
+        Vector3 colliderCenter = playerCollider.bounds.center;
 
-        // Make ground collider only take up half of the player
-        colliderCenter.y = colliderCenter.y - (colliderSize.y / 4f);
-        colliderSize.y = colliderSize.y / 2f;
+        // Make ray box only take up the bottom half of the player
+        colliderCenter.y -= (colliderSize.y / 4f);
+        colliderSize.y /= 2f;
 
+        // make ray box width a little less than player width so the player cant jump when hugging a wall
+        colliderSize.x = colliderSize.x - 0.2f;
 
+        // Create a ray box, return true if the ray is colliding with "Ground" LayerMask
         RaycastHit2D rayHit = Physics2D.BoxCast(colliderCenter, colliderSize, 0f, Vector2.down, extraHeightTest, platformLayerMask);
-        //Debug.Log(rayHit.collider);
+
         return rayHit.collider != null;
 
 
